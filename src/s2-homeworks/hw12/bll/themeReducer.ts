5c597ea344f0739e116a6ef075dab87a62b7ed43
artFetisov <fetishfestoff@mail.ru>
2024-05-28 22:21:13 +0300

    hw12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,32 @@
-const initState = {
-    themeId: 1,
+export enum ThemeEnumAction {
+  SET_THEME_ID = 'SET_THEME_ID',
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export interface SetThemeAction {
+  type: ThemeEnumAction
+  themeId: number
+}
 
-        default:
-            return state
+export interface IThemeState {
+  themeId: number
+}
+
+const initState: IThemeState = {
+  themeId: 1,
+}
+
+export const themeReducer = (state = initState, action: SetThemeAction): IThemeState => {
+  switch (action.type) {
+    case ThemeEnumAction.SET_THEME_ID: {
+      return {
+        ...state,
+        themeId: action.themeId,
+      }
     }
+
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (themeId: number): SetThemeAction => ({ type: ThemeEnumAction.SET_THEME_ID, themeId }) // fix any
