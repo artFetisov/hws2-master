8c11ea1f1b920b46b6f8dcae7eccbd27602ff62b
artFetisov <fetishfestoff@mail.ru>
2024-05-28 12:22:09 +0300

    hw11

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,51 +1,60 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
-* 1 - передать значения в оба слайдера
-* 2 - дописать типы и логику функции change
-* 3 - сделать стили в соответствии с дизайном
-* */
+ * 1 - передать значения в оба слайдера
+ * 2 - дописать типы и логику функции change
+ * 3 - сделать стили в соответствии с дизайном
+ * */
 
 function HW11() {
-    // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
-    const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+  // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
+  const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
+  const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
-        // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-    }
+  const handleSetFirstValue = (event: Event, number: number | number[]) => {
+    setValue1(number as number)
+  }
 
-    return (
-        <div id={'hw11'}>
-            <div className={s2.hwTitle}>Homework #11</div>
+  const handleSetSecondValue = (event: Event, numbers: number | number[]) => {
+    numbers = numbers as number[]
+    setValue1(numbers[0])
+    setValue2(numbers[1])
+  }
 
-            <div className={s2.hw}>
-                <div className={s.container}>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-single-slider'}
-                            // сделать так чтоб value1 изменялось // пишет студент
+  return (
+    <div id={'hw11'}>
+      <div className={s2.hwTitle}>Homework #11</div>
 
-                        />
-                    </div>
-                    <div className={s.wrapper}>
-                        <span id={'hw11-value-1'} className={s.number}>{value1}</span>
-                        <SuperRange
-                            id={'hw11-double-slider'}
-                            // сделать так чтоб value1/2 изменялось // пишет студент
-
-                        />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
-                    </div>
-                </div>
-            </div>
+      <div className={s2.hw}>
+        <div className={s.container}>
+          <div className={s.wrapper}>
+            <span id={'hw11-value'} className={s.number}>
+              {value1}
+            </span>
+            <SuperRange
+              id={'hw11-single-slider'}
+              value={value1}
+              onChange={handleSetFirstValue}
+              // сделать так чтоб value1 изменялось // пишет студент
+            />
+          </div>
+          <div className={s.wrapper}>
+            <span id={'hw11-value-1'} className={s.number}>
+              {value1}
+            </span>
+            <SuperRange id={'hw11-double-slider'} value={[value1, value2]} onChange={handleSetSecondValue} />
+            <span id={'hw11-value-2'} className={s.number}>
+              {value2}
+            </span>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default HW11
